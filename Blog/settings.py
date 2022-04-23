9d80e713c253dd181f42a79333883b622b82d294
ashish077 <ash.coolest7@gmail.com>
2022-04-22 22:12:44 -0400

    deploying take seems forever

--- Blog/settings.py
+++ Blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-it&g9oad!ao_f=1fhyl^+0#&(lcb+%s8@y#lbs!5%t+5dludr_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://teck-blogs.herokuapp.com','127.0.0.1']
 
 
 # Application definition
